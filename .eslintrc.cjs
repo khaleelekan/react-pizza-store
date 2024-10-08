971dbbff629713ac2c5da1dfba4d667f7b733d50
khalilullahi abdulraheem <kalilulekan@gmail.com>
2024-10-08 14:46:32 +0100

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,9 +13,9 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    // 'react-refresh/only-export-components': [
+    //   'warn',
+    //   { allowConstantExport: true },
+    // ],
   },
 }
